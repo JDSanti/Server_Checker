8b2b09867fde3da042256bec0ec3dd3c507beff1
Jose D. Santiago <jduhamel.santiago@gmail.com>
2022-03-28 15:59:04 -0400

    Update readme

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 #Import Libraries
+#Import Libraries
 import socket
 import datetime
 import yagmail
 import os
 from twilio.rest import Client
 
-#Build a infinite looping script that checks the servers every hour and sends email or text if it reports to be dowmn
+#Yagmail SMTP
 yag = yagmail.SMTP('email', 'password')
 
+#Function to connect socket
 def is_running(site):
     """This function attempts to connect to the given server using a socket.
         Returns: Whether or not it was able to connect to the server."""
@@ -18,17 +20,20 @@ def is_running(site):
     except:
         return False
 
+#Function to log into a txt file
 def save_log(state):
     f = open("server_log.txt", "a")
     f.write(state)
     f.close()
 
+#Function to send email log
 def send_email(site):
     contents = [
     f'There is a problem with {site} as of {date_time}\n'
     ]
     yag.send('sender@gmail.com', 'Server Status', contents)
 
+#Function to send text log
 def send_text(site):
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -40,18 +45,24 @@ def send_text(site):
             to='+15555555555'
         )
 
+#Main Function
 if __name__ == "__main__":
-    while True:
-        site = input('Website to check: ')
+    #Array holding what servers you want to check to see if they respond
+    servers_to_check=["server.com"]
+    #Loop over every server
+    for x in servers_to_check:
+        site = (x)
         date_time = datetime.datetime.now()
         if is_running(f'{site}'):
-            print(f"{site} is running as of {date_time}")
+            #If it does respond, log successful connection
             state = str(f"{site} is running as of {date_time}\n")
+            #Log
             save_log(state)
         else:
-            print(f'There is a problem with {site} as of {date_time}')
+            #If it doesn't respond, log the time and date of issue and send email and text notification
             state = str(f'There is a problem with {site} as of {date_time}\n')
+            #Log
             save_log(state)
+            # Send Email
             send_email(site)
-        if input("Would You like to check another website(Y/N)? ") in {'n', 'N'}:
-            break
+            send_text(site)
